fabbe0a793fd249e09cd65e67da146fc8731a60b
F10-silva <igor.f.silva14@aluno.senai.br>
2025-08-04 10:30:52 -0300

    Update

---
+++ functions/update.sql
@@ -0,0 +1,10 @@
+-- Esse código explica que o funcionário de id 5 teve seu salário atualizado para 7500
+update funcionarios
+set salario = 7500
+where id = 5;
+
+--Esse código atualiza a conclusão do projeto de id 5
+update projetos
+set concluido = true
+where id = 5;
+
